9ed1fdb702935400d8055326657982f8e43c4456
Sergey <relzet69@gmail.com>
2018-10-21 00:03:29 +0300

    implemented FileStats

--- file-stats/src/main/java/com/bobocode/FileStats.java
+++ file-stats/src/main/java/com/bobocode/FileStats.java
@@ -1,10 +1,31 @@
 package com.bobocode;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.IntPredicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
 /**
  * {@link FileStats} provides an API that allow to get character statistic based on text file. All whitespace characters
  * are ignored.
  */
 public class FileStats {
+
+    private static IntPredicate isAlphabetic = c -> ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
+
+    private Map<Character, Long> charCountersMap;
+    private char mostPopularChar;
+
+    public FileStats(Map<Character, Long> charCountersMap, char mostPopularChar) {
+        this.charCountersMap = charCountersMap;
+        this.mostPopularChar = mostPopularChar;
+    }
+
     /**
      * Creates a new immutable {@link FileStats} objects using data from text file received as a parameter.
      *
@@ -12,7 +33,31 @@ public class FileStats {
      * @return new FileStats object created from text file
      */
     public static FileStats from(String fileName) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        try (Stream<String> linesStream = FileReaders.getLinesStream(fileName)) {
+            List<String> lines = linesStream.collect(Collectors.toList());
+            Map<Character, Long> charCountersMap = getCharCountersMap(lines);
+            char mostPopularChar = getMostPopularCharacter(charCountersMap);
+
+            return new FileStats(charCountersMap, mostPopularChar);
+        } catch (Exception e) {
+            throw new FileStatsException(e.getMessage(), e.getCause());
+        }
+    }
+
+    private static Map<Character, Long> getCharCountersMap(List<String> lines) {
+        return lines.stream()
+                .flatMap(line -> line
+                        .chars()
+                        .filter(isAlphabetic)
+                        .mapToObj(c -> (char) c))
+                .collect(groupingBy(identity(), counting()));
+    }
+
+    private static char getMostPopularCharacter(Map<Character, Long> charCountersMap) {
+        return charCountersMap.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .orElseThrow()
+                .getKey();
     }
 
     /**
@@ -22,7 +67,7 @@ public class FileStats {
      * @return a number that shows how many times this character appeared in a text file
      */
     public int getCharCount(char character) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        return charCountersMap.getOrDefault(character, 0L).intValue();
     }
 
     /**
@@ -31,7 +76,7 @@ public class FileStats {
      * @return the most frequently appeared character
      */
     public char getMostPopularCharacter() {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        return mostPopularChar;
     }
 
     /**
@@ -41,6 +86,6 @@ public class FileStats {
      * @return {@code true} if this character has appeared in the text, and {@code false} otherwise
      */
     public boolean containsCharacter(char character) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        return charCountersMap.containsKey(character);
     }
 }
--- file-stats/src/main/java/com/bobocode/FileStatsException.java
+++ file-stats/src/main/java/com/bobocode/FileStatsException.java
@@ -1,10 +1,6 @@
 package com.bobocode;
 
 public class FileStatsException extends RuntimeException{
-    public FileStatsException(String message) {
-        super(message);
-    }
-
     public FileStatsException(String message, Throwable cause) {
         super(message, cause);
     }
