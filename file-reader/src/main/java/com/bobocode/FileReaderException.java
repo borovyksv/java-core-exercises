ec9ef8a398db225fff3c7a128086774151b0c72f
Sergey <relzet69@gmail.com>
2018-10-20 21:01:02 +0300

    refactored FileReaders

---
+++ file-reader/src/main/java/com/bobocode/FileReaderException.java
@@ -0,0 +1,7 @@
+package com.bobocode;
+
+class FileReaderException extends RuntimeException {
+    FileReaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- file-reader/src/main/java/com/bobocode/FileReaders.java
+++ file-reader/src/main/java/com/bobocode/FileReaders.java
@@ -1,5 +1,15 @@
 package com.bobocode;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * {@link FileReaders} privides an API that allow to read whole file into a {@link String} by file name.
  */
@@ -12,6 +22,28 @@ public class FileReaders {
      * @return string that holds whole file content
      */
     public static String readWholeFile(String fileName) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        try(Stream<String> stream = getLinesStream(fileName)) {
+            return stream.collect(Collectors.joining("\n"));
+        }
+    }
+
+    private static Stream<String> getLinesStream(String fileName) {
+        try {
+            return Files.lines(getResourcePath(fileName));
+        } catch (IOException e) {
+            throw new FileReaderException("Can't read resource file", e);
+        }
+    }
+
+    private static Path getResourcePath(String fileName) {
+        try {
+            Objects.requireNonNull(fileName, "Resources filename must not be null");
+            URL resourceUrl = Objects.requireNonNull(
+                    FileReaders.class.getClassLoader().getResource(fileName),
+                    fileName + " - given resource file does not exist");
+            return Paths.get(resourceUrl.toURI());
+        } catch (URISyntaxException | NullPointerException e) {
+            throw new FileReaderException("Wrong resource file URI", e);
+        }
     }
 }
