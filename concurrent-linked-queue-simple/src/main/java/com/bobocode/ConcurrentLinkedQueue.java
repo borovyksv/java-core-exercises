96e819def963a17ab1c3a35eec4fd8b1b623e272
Sergey <relzet69@gmail.com>
2018-10-20 18:51:36 +0300

    implemented ConcurrentLinkedQueue

--- concurrent-linked-queue-simple/src/main/java/com/bobocode/ConcurrentLinkedQueue.java
+++ concurrent-linked-queue-simple/src/main/java/com/bobocode/ConcurrentLinkedQueue.java
@@ -1,5 +1,7 @@
 package com.bobocode;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * This queue should be implemented using generic liked nodes. E.g. class Node<T>. In addition, this specific
  * should be thread-safe, which means that queue can be used by different threads simultaneously, and should work correct.
@@ -7,23 +9,54 @@ package com.bobocode;
  * @param <T> a generic parameter
  */
 public class ConcurrentLinkedQueue<T> implements Queue<T> {
+
+    private Node<T> first;
+    private Node<T> last;
+    private AtomicInteger size = new AtomicInteger(0);
+
     @Override
-    public void add(T element) {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+    synchronized public void add(T element) {
+        Node<T> newNode = new Node<>(element);
+        if (first == null) {
+            first = last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+        size.getAndIncrement();
     }
 
     @Override
-    public T poll() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+    synchronized public T poll() {
+        if (first == null) {
+            return null;
+        } else {
+            T element = first.element;
+            first = first.next;
+            if (first == null) {
+                last = null;
+            }
+            size.getAndDecrement();
+            return element;
+        }
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        return size.get();
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        return first == null;
+    }
+
+    private class Node<T> {
+        private T element;
+        private Node<T> next;
+
+        public Node(T element) {
+            this.element = element;
+        }
     }
 }
